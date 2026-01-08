9c6793b4d6cd02d996e759c56e807e2469c0f9cb
Ankit-0018 <as1777766@gmail.com>
2026-01-08 19:25:02 +0530

    Inital setup done

---
+++ backend/src/index.ts
@@ -0,0 +1,16 @@
+import express from 'express';
+import 'dotenv/config';
+
+
+const app = express();
+
+app.get('/', (req , res) => {
+    res.status(200).json({
+        status : "healthy"
+    })
+})
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+})
